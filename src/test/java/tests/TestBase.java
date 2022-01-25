a45ca987ccbec9a9174645d38df98a384f94aeee
Mykola <mykola.danylo@izzi.software>
2022-01-25 16:10:26 +0200

    add selenoid

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -11,5 +12,12 @@ public class TestBase {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
+
 }
