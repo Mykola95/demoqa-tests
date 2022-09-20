d4f4a40e98900cc9613e6ef393993ceba2f9b89e
Mykola <danylo.mykola@gmail.com>
2022-09-20 16:37:32 +0300

    update BaseTest

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -18,22 +18,21 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        Configuration.baseUrl = "https://demoqa.com";
 
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "91");
+        Configuration.browserSize = System.getProperty("browserSize", "1280x1024");
         String login = System.getProperty("login");
         String password = System.getProperty("password");
         String url = System.getProperty("url");
 
-        if (System.getProperty("remote") != null) {
-            Configuration.remote = "https://" + login + ":" + password + "@" + url;
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-        }
+        Configuration.remote = "https://" + login + ":" + password + "@" + url;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "91");
-        Configuration.browserSize = System.getProperty("browserSize", "1280x1024");
 
 
 
