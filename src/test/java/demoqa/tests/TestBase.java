dae1882c380c3973e41da0c044c73cf2a667949d
Mykola <danylo.mykola@gmail.com>
2022-11-17 14:56:41 +0200

    some changes

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -14,47 +14,47 @@ import static demoqa.properties.Property.*;
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
-//    @BeforeAll
-//    static void setUp() {
-//        SelenideLogger.addListener("allure", new AllureSelenide());
-//
-//        Configuration.baseUrl = "https://demoqa.com";
-//
-//        String login = System.getProperty("login");
-//        String password = System.getProperty("password");
-//        String url = System.getProperty("url");
-//
-//        if (url != null) {
-//            Configuration.remote = "https://" + login + ":" + password + "@" + url;
-//
-//            DesiredCapabilities capabilities = new DesiredCapabilities();
-//            capabilities.setCapability("enableVNC", true);
-//            capabilities.setCapability("enableVideo", true);
-//            Configuration.browserCapabilities = capabilities;
-//
-//            Configuration.browser = System.getProperty("browser", "chrome");
-//            Configuration.browserVersion = System.getProperty("browserVersion", "91");
-//            Configuration.browserSize = System.getProperty("browserSize", "1280x1024");
-//        }
-//    }
-
     @BeforeAll
-    static void configure() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = browser();
-        Configuration.browserVersion = browserVersion();
-        Configuration.browserSize = browserSize();
-        if (!remoteUrl().equals("")) {
-            Configuration.remote = remoteUrl() + "https://" + login() + ":" + password() + "@" + url();
+
+        String login = System.getProperty("login");
+        String password = System.getProperty("password");
+        String url = System.getProperty("url");
+
+        if (url != null) {
+            Configuration.remote = "https://" + login + ":" + password + "@" + url;
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+
+            Configuration.browser = System.getProperty("browser", "chrome");
+            Configuration.browserVersion = System.getProperty("browserVersion", "91");
+            Configuration.browserSize = System.getProperty("browserSize", "1280x1024");
         }
     }
+
+//    @BeforeAll
+//    static void configure() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.browser = browser();
+//        Configuration.browserVersion = browserVersion();
+//        Configuration.browserSize = browserSize();
+//        if (!remoteUrl().equals("")) {
+//            Configuration.remote = remoteUrl() + "https://" + login() + ":" + password() + "@" + url();
+//        }
+//    }
 
     @AfterEach
     void addAttachments() {
